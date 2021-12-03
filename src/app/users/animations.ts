0b0aa2a75fdb43d2a633b2dd6efc859228e40097
Shubham Jain <shubham3399z@gmail.com>
2021-12-04 00:32:53 +0530

    toastr

--- src/app/users/animations.ts
+++ src/app/users/animations.ts
@@ -1,4 +1,4 @@
-import { trigger, transition, style, query, group, animateChild, animate, keyframes, } from '@angular/animations';
+import { trigger, transition, style, query,animate, keyframes, } from '@angular/animations';
 export const fader =
   trigger('routeAnimations', [
     transition('* <=> *', [
@@ -11,10 +11,10 @@ export const fader =
           opacity: 0,
           transform: 'scale(0) translateY(100%)',
         }),
-      ]),
+      ],{optional: true}),
       // Animate the new page in
       query(':enter', [
         animate('600ms ease', style({ opacity: 1, transform: 'scale(1) translateY(0)' })),
-      ])
+      ],{optional: true})
     ]),
 ]);
